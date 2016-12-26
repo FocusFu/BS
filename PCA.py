4fb57db95761629e5a7d68b22c468d12a05d2304
fuzhiqiang1230 <fuzhiqiang1230@outlook.com>
2016-12-26 21:44:22 +0800

    稳步前进

--- PCA.py
+++ PCA.py
@@ -1,3 +1,7 @@
 # -*- coding: UTF-8 -*-
-#测试
-print "你好，世界"
+#这是主成分分析算法的一个模块
+'''这是一个降维函数，输入：
+data——要降维的原始数据
+aim——降到目标维度
+返回降维后的数据'''
+def reduceDimension(data,aim):
--- openpicture.py
+++ openpicture.py
@@ -1,2 +1,7 @@
 # -*- coding: UTF-8 -*-
 #这个用来打开图片
+import numpy
+import scipy
+from scipy.misc import imread
+im=imread('Caltech101/1/obj1__0.png')#读入一个图片
+im=imread('Caltech101/2/obj2__0.png')#读入一个图片
