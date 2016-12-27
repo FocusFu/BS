e9ab02029fd853587885580fe19ab831195e510f
fuzhiqiang1230 <fuzhiqiang1230@outlook.com>
2016-12-27 19:44:29 +0800

    PCA算法基本搞定

---
+++ KNN.py
@@ -0,0 +1,4 @@
+# -*- coding: UTF-8 -*-
+#这个是KNN算法文件
+def KNNA(data,k):
+    return
--- openpicture.py
+++ openpicture.py
@@ -2,6 +2,16 @@
 #这个用来打开图片
 import numpy
 import scipy
+import PCA
 from scipy.misc import imread
 im=imread('Caltech101/1/obj1__0.png')#读入一个图片
 im=imread('Caltech101/2/obj2__0.png')#读入一个图片
+a=numpy.array(im,dtype=int)
+print a[64]
+a,b=PCA.PCA(a,1)
+a=numpy.array(a,dtype=float)
+b=numpy.array(b,dtype=int)
+#print a
+print b[64]
+print numpy.shape(b)
+print numpy.shape(a)
