f2a33d99d21fcd117e3e5e63b474840d9ebc3a8b
fuzhiqiang1230 <fuzhiqiang1230@outlook.com>
2016-12-28 16:52:36 +0800

    批量打开图片，修改文件名

--- clustering.py
+++ clustering.py
@@ -1,3 +1,5 @@
 # -*- coding: UTF-8 -*-‘
-#聚类算法
+#聚类算法暂时使用k-means
+import numpy as np
 def cluster(dataList,k):
+    pass
--- main.py
+++ main.py
@@ -1,2 +1,10 @@
 # -*- coding: UTF-8 -*-
 #这是主要的程序文件
+import PCA
+import openpicture
+import clustering
+import KPCA
+import LaE
+import LDA
+import LLE
+import changename
